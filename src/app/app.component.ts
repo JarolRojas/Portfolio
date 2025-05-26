1130e591c191a0bde00a19f308aef67ef6038aeb
Jarol Rojas <jr.rojas0327@gmail.com>
2025-05-26 22:32:26 +0200

    Move the shared folder to components and install and integrate Font Awesome.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { Title } from '@angular/platform-browser';
-import { NavbarComponent } from "./shared/navbar/navbar.component";
 import { RouterOutlet } from '@angular/router';
 import { TranslatorComponent } from './components/translator/translator.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
