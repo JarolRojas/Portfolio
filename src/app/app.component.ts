f381f6e36a243160e3a60fb230515a3103d1b81d
Jarol Rojas <jr.rojas0327@gmail.com>
2025-05-26 23:24:48 +0200

    sections created and routes established

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { Title } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
-import { TranslatorComponent } from './components/translator/translator.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [TranslateModule, RouterOutlet, NavbarComponent, TranslatorComponent],
+  imports: [TranslateModule, RouterOutlet, NavbarComponent],
   providers: [Title],
 })
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,32 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
   },
+  {
+    path: 'experience',
+    loadComponent: () => import('./pages/experience/experience.component').then(m => m.ExperienceComponent)
+  },
+  {
+    path: 'education',
+    loadComponent: () => import('./pages/education/education.component').then(m => m.EducationComponent)
+  },
+  {
+    path: 'project',
+    loadComponent: () => import('./pages/project/project.component').then(m => m.ProjectComponent)
+  },
+  {
+    path: 'skill',
+    loadComponent: () => import('./pages/skill/skill.component').then(m => m.SkillComponent)
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./pages/contact/contact.component').then(m => m.ContactComponent)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
 ];
