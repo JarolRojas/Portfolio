83c08e7da6e2f9f9ad7fde8ae77f223a5950cf34
Jarol Rojas <jr.rojas0327@gmail.com>
2025-05-27 09:45:26 +0200

    Translator fixed and implemented in the header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
+import { Component, inject } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { Title } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { TranslatorComponent } from "./components/shared/translator/translator.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [TranslateModule, RouterOutlet, NavbarComponent],
+  imports: [TranslateModule, RouterOutlet, NavbarComponent, TranslatorComponent],
   providers: [Title],
 })
 export class AppComponent {
-
+  translate = inject(TranslateService);
+  constructor() {
+    this.translate.setDefaultLang('en');
+  }
 }
---
+++ src/app/components/shared/translator/translator.component.ts
@@ -0,0 +1,38 @@
+import { Component, inject } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-translator',
+  imports: [],
+  templateUrl: './translator.component.html',
+})
+export class TranslatorComponent {
+  translate = inject(TranslateService);
+  lang: string;
+
+  constructor() {
+    const savedLang = localStorage.getItem('lang');
+    this.lang = savedLang || 'en'; // 'es' por defecto ahora
+    this.translate.use(this.lang);
+    localStorage.setItem('lang', this.lang);
+    document.documentElement.lang = this.lang;
+  }
+
+  get currentLang() {
+    return this.translate.currentLang || localStorage.getItem('lang') || 'es';
+  }
+
+  // Cambia el idioma a partir de un evento (select)
+  changeLang(event: Event) {
+    const lang = (event.target as HTMLSelectElement).value;
+    this.translate.use(lang);
+    localStorage.setItem('lang', lang);
+  }
+
+  changeLangDirect(lang: string) {
+    this.lang = lang; // Actualiza la variable local
+    this.translate.use(lang);
+    localStorage.setItem('lang', lang); // Guarda el idioma en localStorage
+    document.documentElement.lang = lang; // Cambia el atributo 'lang' del HTML
+  }
+}
