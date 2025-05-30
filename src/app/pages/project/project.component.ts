57e2f20f764fd8bed83f59f13baac40f9f07ad78
Jarol Rojas <jr.rojas0327@gmail.com>
2025-05-30 14:08:06 +0200

    marked installed

--- src/app/pages/project/project.component.ts
+++ src/app/pages/project/project.component.ts
@@ -1,10 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CardComponent } from "../../components/project/card-project/card-project.component";
 import { CardAccountComponent } from "../../components/project/card-account/card-account.component";
 
 @Component({
   selector: 'app-project',
-  imports: [CardComponent, CardAccountComponent],
+  imports: [CardAccountComponent],
   templateUrl: './project.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
