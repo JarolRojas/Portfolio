00955761c7d9bd7155790e14a038b1900801fe9c
Jarol Rojas <jr.rojas0327@gmail.com>
2025-06-02 15:25:39 +0200

    estrcutura del header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslatorComponent } from "./components/shared/translator/translator.component";
+import { FooterComponent } from "./components/shared/footer/footer.component";
+import { NavbarComponent } from "./components/shared/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslatorComponent, FooterComponent, NavbarComponent],
   templateUrl: './app.html'
 })
 export class App {}
